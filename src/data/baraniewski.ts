35befe01ef58a402ea39e8d682f2e7a819c8d92d
Jan Baraniewski <janekbaraniewski@gmail.com>
2020-11-17 00:57:38 +0100

    Add shortDescription to site info

--- src/data/baraniewski.ts
+++ src/data/baraniewski.ts
@@ -1,9 +1,12 @@
 import { Project } from '@/types'
 
 export default {
-  name: 'this-site',
+  name: 'baraniewski.com',
+  shortDescription: 'This site',
   description: `
-This page is created using VueJS, and running on Github Pages.
+
+This page is created using VueJS and running on Github Pages.
+
 `,
   url: 'https://github.com/janekbaraniewski/janekbaraniewski.github.io'
 } as Project
